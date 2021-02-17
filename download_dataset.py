9569797ad5dab7f73d7231d18f3b22c5a8034f06
Martin Lund Haug <martlh@stud.ntnu.no>
2021-02-17 16:09:03 +0100

    changed names in dataset class cifar10

--- download_dataset.py
+++ download_dataset.py
@@ -12,8 +12,8 @@ def get_dataset(name):
 def get_CIFAR10():
     data_tr = datasets.CIFAR10('./CIFAR10', train=True, download=True)
     data_te = datasets.CIFAR10('./CIFAR10', train=False, download=True)
-    X_tr = data_tr.train_data
-    Y_tr = torch.from_numpy(np.array(data_tr.train_labels))
-    X_te = data_te.test_data
-    Y_te = torch.from_numpy(np.array(data_te.test_labels))
+    X_tr = data_tr.data
+    Y_tr = torch.from_numpy(np.array(data_tr.targets))
+    X_te = data_te.data
+    Y_te = torch.from_numpy(np.array(data_te.targets))
     return X_tr, Y_tr, X_te, Y_te
