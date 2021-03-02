ddebac5feaf0ec4d1a0003d53798183e68260a67
Martin Lund Haug <martlh@stud.ntnu.no>
2021-03-02 15:55:38 +0100

    add dir for strategies

---
+++ query_strategies/random_strategy.py
@@ -0,0 +1,14 @@
+import numpy as np
+from .strategy import Strategy
+import torch
+
+
+class Random_Strategy(Strategy):
+    def __init__(self, ALD, net, args):
+        super().__init__(ALD, net, args)
+        self.ALD = ALD
+
+    def query(self, num_query, n_pool):
+        idx_ulb = self.ALD.index['unlabeled']
+        query = np.random.choice(idx_ulb, num_query, replace=False)
+        return query
