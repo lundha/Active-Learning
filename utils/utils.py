171bc18be1eda4a3f7c7f059006f5d62ad2f4301
Martin Haug <martlh@stud.ntnu.no>
2021-04-14 15:36:56 +0200

    div

--- utils/utils.py
+++ utils/utils.py
@@ -2,7 +2,7 @@
 from dataloader import DataSet, Resize, Normalize, ToTensor, Convert2RGB
 from torchvision import transforms, utils
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from skimage import io, transform
 import matplotlib.pyplot as plt
 from datetime import datetime
@@ -35,6 +35,26 @@ def load_data_pool(train=False, arg=None) -> DataSet:
         print(f"Could not load dataset, exception: {e}")
 
     return dataset
+
+class my_subset(Dataset):
+    r"""
+    Subset of a dataset at specified indices.
+
+    Arguments:
+        dataset (Dataset): The whole Dataset
+        indices (sequence): Indices in the whole set selected for subset
+        labels(sequence) : targets as required for the indices. will be the same length as indices
+    """
+    def __init__(self, dataset, indices, labels):
+        self.dataset = dataset
+        self.indices = indices
+        self.labels = labels
+
+    def __getitem__(self, idx):
+         return self.dataset[self.indices[idx]]
+
+    def __len__(self):
+        return len(self.indices)
 
 def print_images(idxs, cols, rows):
 
